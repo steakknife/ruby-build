3bd7309197a10aa0f5cd970d4184bb459463e014
Barry Allard <barry.allard@gmail.com>
2012-08-24 07:57:00 +0000

    ruby-build-standalone-uninstall
    readme uninstall notes

--- install.sh
+++ install.sh
@@ -1,23 +1,2 @@
 #!/bin/sh
-
-set -e
-
-if [ -z "${PREFIX}" ]; then
-  PREFIX="/usr/local"
-fi
-
-BIN_PATH="${PREFIX}/bin"
-SHARE_PATH="${PREFIX}/share/ruby-build"
-
-mkdir -p "${BIN_PATH}"
-mkdir -p "${SHARE_PATH}"
-
-for file in bin/*; do
-  cp "${file}" "${BIN_PATH}"
-done
-
-for file in share/ruby-build/*; do
-  cp "${file}" "${SHARE_PATH}"
-done
-
-echo "Installed ruby-build at ${PREFIX}"
+make install
